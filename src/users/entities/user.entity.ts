64311983929f8de9bb4f3233a87a4fe7bc913962
Abdul-Latif <m3yiel@gmail.com>
2025-11-23 20:50:53 +0200

    initial migration

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Roles } from "../../utilities/role-enum";
+
+@Entity('users')
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name: string;
+    @Column()
+    email: string;
+    @Column()
+    password: string;
+    @Column({ type: 'enum', enum: Roles, array: true, default: [Roles.USER] })
+    roles: Roles[];
+}
