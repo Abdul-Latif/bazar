6f456d70e632c2febf40070c4e7e7a199b342bcf
Abdul-Latif <m3yiel@gmail.com>
2025-12-04 23:44:23 +0200

    data source config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from '../db/data-source';
+import { dataSource } from '../db/data-source';
 import { UsersModule } from './users/users.module';
 import { CurrentUserMiddleware } from './utilities/middlewares/current-user.middleware';
 import { CategoryModule } from './category/category.module';
@@ -9,7 +9,14 @@ import { ReviewsModule } from './reviews/reviews.module';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule, CategoryModule, ProductsModule, ReviewsModule, OrdersModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    migrations: ['dist/db/migration/*{.ts,.js}'],
+    synchronize: false,
+    logging: true,
+  }), UsersModule, CategoryModule, ProductsModule, ReviewsModule, OrdersModule],
   controllers: [],
   providers: [],
 })
